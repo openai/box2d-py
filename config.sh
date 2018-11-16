719a110033aa43acc097373d93110b0d9090b8ed
Peter Zhokhov <peterz@openai.com>
2018-11-16 15:42:02 -0800

    multibuild wip

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 function pre_build {
     set -ex
+    build_swig
     pip install .
     cd tests
     pip install --user nose coverage
     python -m nose -v -w . --with-coverage --cover-package=Box2D
     cd ..
-    build_swig
 }
 
