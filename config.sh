bdca993c6c54c556061d656e130769acf5a448a5
Peter Zhokhov <peterz@openai.com>
2018-11-16 16:29:00 -0800

    polishing config.sh

--- config.sh
+++ config.sh
@@ -3,10 +3,8 @@ function pre_build {
     set -ex
     build_swig
     pip install .
-    cd tests
-    pip install --user nose coverage
-    python -m nose -v -w . --with-coverage --cover-package=Box2D
-    cd ..
+    pip install nose coverage
+    nose -v -w tests/ --with-coverage --cover-package=Box2D
 }
 
 function run_tests {
