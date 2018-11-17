4504c143528fc544ce9b55255742febfaff80cd4
Peter Zhokhov <peterz@openai.com>
2018-11-16 16:17:14 -0800

    further testing

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@ set -ex
 
 # if [[ ! -z "$TRAVIS_TAG" ]]; then
     ls -lht ./wheelhouse
-    pip install --user twine
-    # python -m twine upload ./wheelhouse/box2d_py-*
+    pip install twine
+    # twine upload ./wheelhouse/box2d_py-*
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python setup.py sdist       
-        python -m twine upload dist/*
+        twine upload dist/*
     fi
 # fi
 
