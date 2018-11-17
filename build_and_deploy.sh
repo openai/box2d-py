99b98064b74a1897a031c8fa46aed04fd2aaf1ac
Peter Zhokhov <peterz@openai.com>
2018-11-16 16:03:26 -0800

    testing

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -6,7 +6,7 @@ pip install --user nose coverage
 python -m nose -v -w . --with-coverage --cover-package=Box2D
 cd ..
 
-if [[ ! -z "$TRAVIS_TAG" ]]; then
+#if [[ ! -z "$TRAVIS_TAG" ]]; then
     
     # PYPLATFORM=$(python3 get_platform.py)
     pip wheel . --wheel-dir=/tmp/wheelhouse
@@ -15,11 +15,11 @@ if [[ ! -z "$TRAVIS_TAG" ]]; then
     ls -lht /tmp/wheelhouse
 
     pip install --user twine
-    python -m twine upload /tmp/wheelhouse/box2d_py-*
+    # python -m twine upload /tmp/wheelhouse/box2d_py-*
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python setup.py sdist       
-        python -m twine upload dist/*
+        # python -m twine upload dist/*
     fi
-fi
+#fi
 
